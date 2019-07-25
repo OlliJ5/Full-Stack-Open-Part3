c3a4b93c75bc7b5186152ff81bcf8f522a2574a0
Olli Rousu <olli.rousu@hotmail.com>
2019-07-25 16:22:43 +0300

    frontend build to heroku

---
+++ build/precache-manifest.dcc8b33ab592b9be86db0a078c89ef60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38f8407ebe20e0e7ad65bb70a5a8ef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a013f8c3eb59bc29dbb2",
+    "url": "/static/css/main.19774ad0.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "a013f8c3eb59bc29dbb2",
+    "url": "/static/js/main.48efbed7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
