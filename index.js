30e9ac3c421346dbe468012ce201695cae607ff8
Olli Rousu <olli.rousu@hotmail.com>
2019-07-26 17:32:29 +0300

    3.19 - 3.20 done

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!body.name) {
@@ -44,6 +44,7 @@ app.post('/api/persons', (req, res) => {
   person.save().then(saved => {
     res.json(saved.toJSON())
   })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
@@ -86,6 +87,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
